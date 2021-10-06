229e32e8a7edb3be9b323027349e6f59a9283335
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-10-06 16:52:41 +0100

    move to gradle project

---
+++ src/main/java/dev/tomheaton/ForestLogger.java
@@ -0,0 +1,41 @@
+package dev.tomheaton;
+import dev.tomheaton.util.Colours;
+
+public class ForestLogger {
+
+    private final String name;
+    private final String prefix;
+
+    public ForestLogger() {
+        this("default");
+    }
+
+    public ForestLogger(String name) {
+        this(name, "[" + name + "]");
+    }
+
+    public ForestLogger(String name, String prefix) {
+        this.name = name;
+        this.prefix = prefix;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    public void info(String message) {
+        System.out.println(Colours.ANSI_RED + message);
+    }
+
+    public void error(String message) {
+        System.out.println(message);
+    }
+
+    public void warn(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/dev/tomheaton/Main.java
@@ -0,0 +1,9 @@
+package dev.tomheaton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ForestLogger logger = new ForestLogger("test");
+        logger.info("lets go");
+    }
+}
